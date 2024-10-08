8f67ec4bab05f146acf4a19de9b6e4be4ddef35a
Ananda Puja Wandra <anandapujawandra@192.168.1.5>
2024-10-08 20:42:38 +0700

    initial setup countries server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 
-const app = new Hono()
+const app = new Hono();
+const api = app.basePath("/api");
+app.use(logger());
 
-app.get('/', (c) => {
-  return c.text('Hello Country!')
+api.get('/', (c) => {
+  return c.text('Hello Country!');
 })
 
-export default app
+export default app;
